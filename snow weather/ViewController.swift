3df58404c608e0cf819259bdb08d0e4a2cdd804d
王松毅 <oumatsutakeshi@wangsongyideMacBook-Air.local>
2014-07-31 08:51:07 +0800

    Initial Commit

---
+++ snow weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snow weather
+//
+//  Created by 王松毅 on 14-7-31.
+//  Copyright (c) 2014年 王松毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ snow weatherTests/snow_weatherTests.swift
@@ -0,0 +1,35 @@
+//
+//  snow_weatherTests.swift
+//  snow weatherTests
+//
+//  Created by 王松毅 on 14-7-31.
+//  Copyright (c) 2014年 王松毅. All rights reserved.
+//
+
+import XCTest
+
+class snow_weatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
